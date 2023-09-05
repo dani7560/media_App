279aee4c238fbc466eefbcbe7b3b98820ae7bc45
dani7560 <danieldesisa75@gmail.com>
2023-09-06 01:54:45 +0900

    media app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Container } from '@material-ui/core';
+import Navbar from './componenets/Navbar/Navbar';
+import Home from './componenets/Home/Home';
+import {BrowserRouter, Router, Route, Routes} from 'react-router-dom'
+import Auth from './componenets/Auth/Auth';
 
-function App() {
+const App = () => {
+ 
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Container maxWidth="lg">
+        <Navbar />
+        {/* Use Routes to wrap your Route components */}
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/auth' element={<Auth />} />
+        </Routes>
+      </Container>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
